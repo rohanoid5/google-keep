b9ffdbe8c248201ec3d6cd5c7e73ea09d00ab887
Rohan Dey <rohanoid5@gmail.com>
2020-01-08 01:13:44 +0530

    Updates

---
+++ precache-manifest.e97b82f52530ede73483cef50f41ce90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9f2d6aa245da4ef7b8cd42e90b474c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65d180a3b02f20d57229",
+    "url": "/static/css/main.404d1ef2.chunk.css"
+  },
+  {
+    "revision": "ae682b1b54e3c9eba4d4",
+    "url": "/static/js/2.6dd18ad8.chunk.js"
+  },
+  {
+    "revision": "d5cab2694bbc8e1d611856c0daeb00cc",
+    "url": "/static/js/2.6dd18ad8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65d180a3b02f20d57229",
+    "url": "/static/js/main.985f4aa7.chunk.js"
+  },
+  {
+    "revision": "a41c8c01e2266500f456",
+    "url": "/static/js/runtime-main.5422d99f.js"
+  }
+]);
