7b9d7e6098df0e83988d60508429cb467d100585
Rohan Dey <rohanoid5@gmail.com>
2020-01-08 11:52:12 +0530

    Minor CSS fixes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,8 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 import autosize from 'autosize';
 import localforage from 'localforage';
-import { DEFAULT_STATE } from '../utils/constants';
+import { DEFAULT_STATE, DARK_THEME } from '../utils/constants';
 import Note from './Note';
+import ThemeContext from '../contexts/ThemeContext';
 
 const Notes = ({ notes, onRemove, onArchive, onUpdate, onStar }) => {
   const [selectedNote, setSelectedNote] = useState(DEFAULT_STATE);
@@ -43,82 +44,104 @@ const Notes = ({ notes, onRemove, onArchive, onUpdate, onStar }) => {
   };
 
   return (
-    <div>
-      {tagVisibility && <div className="note-tag">STARRED</div>}
-      <div className="notes">
-        {notes
-          .filter(item => item.isStarred)
-          .map(item => {
-            return (
-              <Note
-                key={item.id}
-                className="note"
-                note={item}
-                onRemove={onRemove}
-                onArchive={onArchive}
-                onStar={onStar}
-                setModalVisibility={setModalVisibility}
-                setSelectedNote={setSelectedNote}
-              />
-            );
-          })}
-        {modalVisibility}
-      </div>
-      {tagVisibility && (
-        <div style={{ marginTop: '8px' }} className="note-tag">
-          OTHERS
-        </div>
-      )}
-      <div className="notes">
-        {notes
-          .filter(item => !item.isStarred)
-          .map(item => {
-            return (
-              <Note
-                key={item.id}
-                className="note"
-                note={item}
-                onRemove={onRemove}
-                onArchive={onArchive}
-                onStar={onStar}
-                setModalVisibility={setModalVisibility}
-                setSelectedNote={setSelectedNote}
-              />
-            );
-          })}
-        {modalVisibility}
-      </div>
-      <div className={modalVisibility ? 'modale opened' : 'modale'}>
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <input
-              onChange={onNoteChange}
-              value={selectedNote.title}
-              className="note-title-input"
-              type="text"
-              name="title"
-              placeholder="Title"
-            />
-            <textarea
-              onChange={onNoteChange}
-              value={selectedNote.body}
-              name="body"
-              ref={contentInput}
-              rows={1}
-              className="note-content-input"
-              type="text"
-              placeholder="Take a note..."
-            ></textarea>
-            <button
-              className="modal-btn note-btn"
-              onClick={() => setModalVisibility(false)}
+    <ThemeContext.Consumer>
+      {({ theme }) => (
+        <div>
+          {tagVisibility && <div className="note-tag">STARRED</div>}
+          <div className="notes">
+            {notes
+              .filter(item => item.isStarred)
+              .map(item => {
+                return (
+                  <Note
+                    key={item.id}
+                    className="note"
+                    note={item}
+                    onRemove={onRemove}
+                    onArchive={onArchive}
+                    onStar={onStar}
+                    setModalVisibility={setModalVisibility}
+                    setSelectedNote={setSelectedNote}
+                  />
+                );
+              })}
+            {modalVisibility}
+          </div>
+          {tagVisibility && (
+            <div style={{ marginTop: '8px' }} className="note-tag">
+              OTHERS
+            </div>
+          )}
+          <div className="notes">
+            {notes
+              .filter(item => !item.isStarred)
+              .map(item => {
+                return (
+                  <Note
+                    key={item.id}
+                    className="note"
+                    note={item}
+                    onRemove={onRemove}
+                    onArchive={onArchive}
+                    onStar={onStar}
+                    setModalVisibility={setModalVisibility}
+                    setSelectedNote={setSelectedNote}
+                  />
+                );
+              })}
+            {modalVisibility}
+          </div>
+          <div className={modalVisibility ? 'modale opened' : 'modale'}>
+            <div
+              className={
+                theme === DARK_THEME
+                  ? 'modal-dialog'
+                  : 'modal-dialog modal-dialog-light'
+              }
             >
-              Close
-            </button>
+              <div className="modal-content">
+                <input
+                  onChange={onNoteChange}
+                  value={selectedNote.title}
+                  className={
+                    theme === DARK_THEME
+                      ? 'note-title-input'
+                      : 'note-title-input note-title-input-light'
+                  }
+                  type="text"
+                  name="title"
+                  placeholder="Title"
+                />
+                <textarea
+                  onChange={onNoteChange}
+                  value={selectedNote.body}
+                  name="body"
+                  ref={contentInput}
+                  rows={1}
+                  className={
+                    theme === DARK_THEME
+                      ? 'note-content-input'
+                      : 'note-content-input note-content-input-light'
+                  }
+                  type="text"
+                  placeholder="Take a note..."
+                ></textarea>
+                <button
+                  className={
+                    theme === DARK_THEME
+                      ? 'modal-btn note-btn'
+                      : 'modal-btn note-btn note-btn-light'
+                  }
+                  onClick={() => setModalVisibility(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </ThemeContext.Consumer>
   );
 };
 
